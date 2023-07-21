b2ddaedb5387fc8e810eba6cafe7601b1d62292b
unknown <test@gmail.com>
2023-07-21 14:53:13 +0545

    form validations and google recaptcha

--- src/main/java/com/codewithalex/springmvc/controller/LoginController.java
+++ src/main/java/com/codewithalex/springmvc/controller/LoginController.java
@@ -1,4 +1,6 @@
 package com.codewithalex.springmvc.controller;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,8 +8,11 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.codewithalex.springmvc.model.User;
 import com.codewithalex.springmvc.repository.UserRepository;
+import com.codewithalex.springmvc.utils.VerifyRecaptcha;
 
 @Controller
 public class LoginController {
@@ -24,19 +29,31 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String doLogin(@ModelAttribute User user, Model model) {
+	public String doLogin(@ModelAttribute User user, Model model,@RequestParam("g-recaptcha-response") String reCode) throws IOException {
 		
-		model.addAttribute("uname",user.getUsername());
-		
+		if(VerifyRecaptcha.verify(reCode)) {
+			
+	
 		user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
 
 		User usr =userRepo.findByUsernameAndPassword(user.getUsername(),user.getPassword());
-	
+		
+		
 		if(usr!=null) {
-		return "home";
-	
+			
+			model.addAttribute("uname",user.getUsername());
+
+			return "home";
+		
+			}
+		else {
+			model.addAttribute("message","user not found");
+			return "login";
+			
 		}
-		model.addAttribute("message", "user not found !!!");
+	}
+
+		model.addAttribute("message", "you are not human !!!");
 		return"login";
 		
 		
